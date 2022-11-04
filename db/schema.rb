8dc92c8668b87d63fa32db3ab6296f4fab252914
paosua86 <paolasuarezb1986@gmail.com>
2022-11-03 19:05:25 -0500

    first product added

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_03_235348) do
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "prince"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
