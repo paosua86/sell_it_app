82694442b6e83fad5a0f87b22217c67ed47e4e8f
paosua86 <paolasuarezb1986@gmail.com>
2022-11-04 20:51:27 -0500

    second page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-get '/products', to: 'products#index'
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/products', to: 'products#index'
+  get '/products/:id', to: 'products#show', as: :product
 end
