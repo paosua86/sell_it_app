10d24c59230fd39f26a804013997b0b8a7f5114d
paosua86 <paolasuarezb1986@gmail.com>
2022-11-11 11:30:06 -0500

    add products and redirect

---
+++ test/controllers/products_controler_test.rb
@@ -0,0 +1,39 @@
+require "test_helper"
+
+class ProductsControllertest < ActionDispatch::IntegrationTest
+  test "render a list of products" do
+    get products_path
+
+    assert_response :success
+    assert_select ".product", 2
+  end
+
+  test "render a detailed product page" do
+    get product_path(products(:ps4))
+
+    assert_response :success
+    assert_select ".title", "PS4 Fat"
+    assert_select ".prince", "$ 150"
+    assert_select ".description", "Ps4 good"
+  end
+
+  test "render a new product form" do
+    get new_product_path
+
+    assert_response :success
+    assert_select "form"
+  end
+
+  test "allow to create a new product" do
+    post products_path, params:{
+      product: {
+        title: "nintendo",
+        description: "nothing",
+        prince: 45
+      }
+    }
+
+    assert_redirected_to products_path
+  end
+
+end
